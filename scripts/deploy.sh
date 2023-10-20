5b0da9b5d33173f24f9e1b8c52e964f904ba54a4
amarvil02 <adrianmartinezvilchez02@gmail.com>
2023-10-20 12:37:07 +0000

    Configuramos el archivo config.php de aplicación

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,9 @@
 # Muestra todos los comandos que se van ejecutando
 set -x
 
+# Incluimos las variables
+source .env
+
 # Actualizamos los repositorios
 apt update
 
@@ -17,3 +20,8 @@ git clone https://github.com/josejuansanchez/iaw-practica-lamp.git /tmp/iaw-prac
 
 # Movemos el código fuente de la aplicación a /var/www/html
 mv /tmp/iaw-practica-lamp/src/* /var/www/html
+
+# Configuramos el archivo config.php de la aplicación
+sed -i "s/database_name_here/$DB_NAME/" /var/www/html/config.php
+sed -i "s/username_here/$DB_USER/" /var/www/html/config.php
+sed -i "s/password_here/$DB_PASSWORD/" /var/www/html/config.php
